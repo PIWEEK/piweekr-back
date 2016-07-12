f53cf7df12f2313a31509cda4ebe3ffbe0104025
David Barragán Merino <david.barragan@kaleidos.net>
2016-07-12 20:24:32 +0200

    Project sample data

--- src/core/sample_data.py
+++ src/core/sample_data.py
@@ -6,15 +6,26 @@ import uuid
 
 from .users import user_entities
 from .ideas import idea_entities
+from .projects import project_entities
 
 from services.repository.sql import repo
 from services.repository.sql.users import user_repository
 from services.repository.sql.ideas import idea_repository
+from services.repository.sql.projects import project_repository
 
 
 sample_colors = ["#FC8EAC", "#A5694F", "#002e33", "#67CF00", "#71A6D2", "#FFF8E7", "#4B0082", "#007000",
  		 "#40826D", "#708090", "#761CEC", "#0F0F0F", "#D70A53", "#CC0000", "#FFCC00", "#FFFF00",
 		 "#C0FF33", "#B6DA55", "#2099DB"]
+
+sample_technologies = ["python", "groovy", "c", "html", "css", "angualr", "cloujure", "cloujurescript", "IoT",
+                       "music", "UX", "Design", "SASS", "CSS", "HTML", "PostCSS"]
+
+sample_logos = [
+   "http://createfunnylogo.com/blazed/MetanOR.jpg",
+   "http://createfunnylogo.com/logo/techcrunch/Virtual%20Gym.jpg",
+   "http://createfunnylogo.com/logo/flickr/PiWeekr.jpg",
+]
 
 class SampleData():
     sd = SampleData(seed=1234567890)
@@ -23,11 +34,14 @@ class SampleData():
         repo.truncate_all_tables()
 
         self.user_ids = []
+        self.piweek_ids = []
         self.idea_ids = []
+        self.project_ids = []
 
         print("Generating sample data...")
         self.make_users()
         self.make_ideas()
+        self.make_projects()
         print("Done.")
 
     def make_users(self):
@@ -70,4 +84,21 @@ class SampleData():
             )
             idea = idea_repository.create(idea)
             self.idea_ids.append(idea.id)
+
+    def make_projects(self):
+        for i in range(50):
+            project = project_entities.Project(
+                uuid=uuid.uuid4().hex,
+                title=self.sd.words(5, 10).capitalize(),
+                description=self.sd.paragraphs(2, 4),
+                technologies=[self.sd.choice(sample_technologies) for i in range(0, self.sd.int(0, 5))],
+                needs=self.sd.paragraphs(2, 4),
+                logo=self.sd.choice(sample_logos),
+                piweek_id=1,
+                idea_from_id=random.choice(self.idea_ids),
+                owner_id=random.choice(self.user_ids),
+                created_at=arrow.get(self.sd.past_datetime()),
+            )
+            project = project_repository.create(project)
+            self.project_ids.append(project.id)
 
