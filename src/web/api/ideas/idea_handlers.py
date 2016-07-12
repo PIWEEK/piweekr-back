80d6a0c81851dd66bfc64afa3e81d448c2afd03b
Andrés Moya <andres.moya@kaleidos.net>
2016-07-12 12:47:30 +0200

    Go back to hardcoded ideas

--- src/web/api/ideas/idea_handlers.py
+++ src/web/api/ideas/idea_handlers.py
@@ -7,11 +7,11 @@ from tools.adt.converter import to_plain, from_plain
 
 # @login_required
 def list_ideas(request):
-    ideas = idea_actions.list_ideas()
-    return responses.Ok([
-        to_plain(idea, ignore_fields=["id"])
-        for idea in ideas
-    ])
+    # ideas = idea_actions.list_ideas()
+    # return responses.Ok([
+    #     to_plain(idea, ignore_fields=["id"])
+    #     for idea in ideas
+    # ])
 
     return responses.Ok([
         {
