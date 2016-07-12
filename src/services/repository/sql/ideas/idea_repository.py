750789b184888d63ddc6958c98d8d3a624499159
David Barragán Merino <david.barragan@kaleidos.net>
2016-07-12 20:24:01 +0200

    flake8

--- src/services/repository/sql/ideas/idea_repository.py
+++ src/services/repository/sql/ideas/idea_repository.py
@@ -4,14 +4,14 @@ from sqlalchemy import Column
 from sqlalchemy.sql import select, join
 from sqlalchemy.dialects.postgresql import JSONB
 
-from tools.adt.adt_sql import SQLADTRepository
-
 from core.ideas import idea_entities
 from core.users import user_entities
 
 from services.repository.sql import repo
 
-repo.add_adt_table(idea_entities.Idea, "ideas",
+repo.add_adt_table(
+    idea_entities.Idea,
+    "ideas",
     manual_columns={
         "reactions_counts": Column("reactions_counts", JSONB),
     }
@@ -26,13 +26,20 @@ def create(idea):
 
 def list():
     with repo.context() as context:
-        ideas = repo.retrieve_joined_adts(context,
-            idea_entities.Idea, {"ideas": idea_entities.Idea, "users": user_entities.User},
-            select([repo.ideas, repo.users], use_labels=True)
-                .select_from(join(
-                    repo.ideas, repo.users, repo.ideas.c.owner_id == repo.users.c.id
-                ))
-                .order_by(repo.ideas.c.title)
+        ideas = repo.retrieve_joined_adts(
+            context,
+            idea_entities.Idea,
+            {"ideas": idea_entities.Idea, "users": user_entities.User},
+            select(
+                [repo.ideas, repo.users],
+                use_labels=True
+            )
+            .select_from(
+                join(
+                    repo.ideas,
+                    repo.users,
+                    repo.ideas.c.owner_id == repo.users.c.id
+                )
+            ).order_by(repo.ideas.c.title)
         )
     return ideas
-
